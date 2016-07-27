8497ab3ba153d3bf5388e5a5922f21ee4ac052fb
VenkateshMoorthi <venkateshmoorthi11@gmail.com>
2016-07-27 09:24:05 +0530

    Add files via upload

--- applyforpurchase.component.ts
+++ applyforpurchase.component.ts
@@ -1,8 +1,43 @@
 ﻿import {Component} from '@angular/core';
+import {ControlGroup, Control, FormBuilder, Validators} from '@angular/common';
+import {HTTP_PROVIDERS} from '@angular/http';
+import {ROUTER_DIRECTIVES} from '@angular/router-deprecated';
+import {FormGroup} from '@angular/forms';
 
 @Component({
 	templateUrl: 'app/applyforpurchase.html',
-	styleUrls:['app/applyforpurchase.css']
+	styleUrls: ['app/applyforpurchase.css'],
+	directives: [ROUTER_DIRECTIVES ],
+	providers: [HTTP_PROVIDERS]
 })
 
-export class ApplyForPurchase { }
+export class ApplyForPurchase {
+	form: ControlGroup;
+	model;
+	
+	propertytypes = ["Single Family", "Townhouse", "Condo",
+		"Cooperative", "2 to 4 units", "Multifamily (>4 units)",
+		"PUD-Attached", "PUD-Detached", "High Rise Condo(>7 stories)",
+		"Manufactored Mobile Home"];	
+	nextattempt = false;
+
+	constructor(fb: FormBuilder) {
+		this.form = fb.group({
+			street: ['', Validators.required],
+			aptunit: [],
+			zipcode: [],
+			city: [],
+			state: [],
+			homevalue: [],
+			propertyusage: [],
+			propertytype: []
+
+		});
+		console.log(this.form);
+	}
+	next(form) {
+		this.nextattempt = true;
+	}
+
+
+}
